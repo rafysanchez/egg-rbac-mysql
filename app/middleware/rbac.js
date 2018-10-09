f12b77e109033bc9784ac9e0813be380c8bda17b
鲁璟洁 <lujj@lujingjies-iMac.local>
2018-10-09 17:25:40 +0800

    改变用户权限验证逻辑，修改菜单全局变量名称

--- app/middleware/rbac.js
+++ app/middleware/rbac.js
@@ -1,50 +1,42 @@
 'use strict';
 
 module.exports = options => {
-  return async function rbac(ctx, next) {
-    if(ctx.path.endsWith('/')){
-        ctx.path = ctx.path.substring(0,ctx.path.length-1);
-    }
-    if(typeof(options.matchPattern) !== 'undefined'){
-        if(ctx.path.match(options.matchPattern)!==null && !options.escapeUrl.includes(ctx.path)){
-            const result = await checkPermission(ctx.session.user, ctx.path, ctx.app.menuPermission)
-            if(result){
+    return async function rbac(ctx, next) {
+        if (ctx.path.endsWith('/')) {
+            ctx.path = ctx.path.substring(0, ctx.path.length - 1);
+        }
+        if (ctx.path.match(options.matchPattern) !== null && !options.escapeUrl.includes(ctx.path)) {
+            const result = await checkPermission(ctx.session.user, ctx.path, ctx.app.liteMenu)
+            if (result === null) {
+                ctx.status = 404;
+            } else if (result === 401) {
+                // ctx.status = 401;
                 await next();
-            }else{
+            } else if (result) {
+                await next();
+            } else {
                 ctx.status = 200;
                 return await ctx.render(options.errorPage, options.errorMessage);
             }
-        }else{
+        } else {
             await next();
         }
-    }else{
-        if(await checkPermission(ctx.session.user, ctx.path, ctx.app.menuPermission)){
-            await next();
-        }else{
-            ctx.status = 200;
-            return await ctx.render(options.errorPage, options.errorMessage);
-        }
-    }
-  };
+    };
 
-  async function checkPermission(user, path, menuPermission){
-    let menuPermissionId = '';
-    for(const menu of menuPermission){
-        if(path === menu.menu_path){
-            menuPermissionId = menu.permission_id;
-            break;
-        }
-    }
-    if(menuPermissionId === '') {return false};
-    let menuPermissionAuth = false;
-    for(const role of user.roles){
-        for(const permission of role.permissions){
-            if(menuPermissionId === permission.id){
-                menuPermissionAuth = true;
+    async function checkPermission(user, path, menus) {
+        let onMenu = null;
+        for (const menu of menus) {
+            if (path === menu.menu_path) {
+                onMenu = menu;
                 break;
             }
         }
+        if (onMenu === null) {
+            return null
+        };
+        if (!user) {
+            return 401;
+        }
+        return user.permissions.includes(onMenu.menu_id);
     }
-    return menuPermissionAuth;
-  }
 };
